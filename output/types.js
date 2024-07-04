7765b3952f22272188ea673d2d24c08fb047980c
Diego <diegompaiva06@gmail.com>
2024-07-03 22:47:31 -0400

    Primeiro commit

---
+++ output/types.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("Hello World");
+console.log("=========");
+const nome = "jão";
+console.log(`Seja bem vindo ${nome}`);
+console.log("==========");
+class Produto {
+    constructor(ProdutoNome, ProdutoValor) {
+        this.nome = ProdutoNome;
+        this.valor = ProdutoValor;
+    }
+}
+console.log("teste de pasta");
+console.log("mais teste");
+//# sourceMappingURL=types.js.map
---
+++ scripts/types.ts
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+console.log("=========");
+
+const nome = "jão";
+
+console.log(`Seja bem vindo ${nome}`);
+
+console.log("==========");
+
+class Produto {
+  nome: string;
+  valor: number;
+
+  constructor(ProdutoNome: string, ProdutoValor: number) {
+    this.nome = ProdutoNome;
+    this.valor = ProdutoValor;
+  }
+}
+
+console.log("teste de pasta");
+console.log("mais teste");
+// Será se vai o comentario?
